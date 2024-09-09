41a7b9a5aa16667fb0d6b5f733806f89cc9c074c
BoiLudens <mattacoker@gmail.com>
2024-09-09 19:24:29 -0400

    added clickable doge

--- player/player_night.gd
+++ player/player_night.gd
@@ -1,6 +1,6 @@
 extends Camera3D
 
-const LOOKAROUND_SPEED: float = 0.01
+const LOOKAROUND_SPEED: float = 0.05
 
 # accumulators
 var rot_x: float  = 0
@@ -8,18 +8,18 @@ var rot_y: float = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	print(Vector3.FORWARD)
+	Input.mouse_mode = Input.MOUSE_MODE_CONFINED
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
 
-func _input(event: InputEvent) -> void:
-	if event is InputEventMouseMotion:
-		# modify accumulated mouse rotation
-		rot_x += event.relative.x * LOOKAROUND_SPEED
-		rot_y += event.relative.y * LOOKAROUND_SPEED
-		transform.basis = Basis() # reset rotation
-		rotate_object_local(Vector3(0, -1, 0), rot_x) # first rotate in Y
-		rotate_object_local(Vector3(-1, 0, 0), rot_y) # then rotate in X
+
+func _physics_process(delta: float) -> void:
+	# TODO: can this be simplified?
+	var input_dir := Input.get_vector("move_left", "move_right", "move_up", "move_down")
+	rot_x += input_dir.x * LOOKAROUND_SPEED
+	transform.basis = Basis() # reset rotation
+	rotate_object_local(Vector3(0, -1, 0), rot_x) # first rotate in Y
